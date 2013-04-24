5a4fc247fdafe08edaddd33418838b189e9566af
jsanjup <josevallejera@gmail.com>
2013-04-24 17:39:37 +0200

    pintando mapas

--- src/com/app/chinastores/MainActivity.java
+++ src/com/app/chinastores/MainActivity.java
@@ -3,8 +3,13 @@ package com.app.chinastores;
 import com.app.chinastores.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -16,9 +21,10 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener{
     private static final int ACTIVITY_CREATE=0;
     private static final int ACTIVITY_EDIT=1;
     private static final int ACTIVITY_READ=2;
@@ -29,6 +35,9 @@ public class MainActivity extends Activity {
     private static final int EXIT_ID= HELP_ID+1;
     private static final int DELETE_ID = EXIT_ID + 1;
     private static final int EDIT_ID = DELETE_ID+1;
+    
+    private LocationManager locationManager;
+	private String provider;
 
     private StoresDbAdapter mDbHelper;
     private ListView list;
@@ -36,11 +45,15 @@ public class MainActivity extends Activity {
     private boolean bazar;
     private Button alim;
     private Button baz;
+    
+    private double lat;
+    private double lon;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        iniciarLocalizador();
         bazar=false;
         setContentView(R.layout.activity_main);
         alim= (Button) findViewById(R.id.ButtonA);
@@ -74,6 +87,26 @@ public class MainActivity extends Activity {
         
         fillData(bazar);
         registerForContextMenu(list);
+    }
+    
+    private void iniciarLocalizador(){
+    	// Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Define the criteria how to select the locatiin provider -> use
+		// default
+		Criteria criteria = new Criteria();
+		provider = locationManager.getBestProvider(criteria, false);
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		// Initialize the location fields
+		if (location != null) {
+			System.out.println("Provider " + provider + " has been selected.");
+			lat = (location.getLatitude());
+			lon = (location.getLongitude());
+		} else {
+			Toast.makeText(this, "Provider not available",
+					Toast.LENGTH_SHORT).show();
+		}
     }
 
     private void fillData(boolean bazar) {
@@ -119,7 +152,7 @@ public class MainActivity extends Activity {
     
     public void vermapa(){
 
-        Intent i = new Intent(this, MapActivity.class);
+        Intent i = new Intent(this, MyMapActivity.class);
         startActivityForResult(i, ACTIVITY_READ);
     }
     
@@ -192,4 +225,28 @@ public class MainActivity extends Activity {
     public double distancia(){
     	return 0;
     }
+    
+    public void onLocationChanged(Location location) {
+		lat =  (location.getLatitude());
+		lon = (location.getLongitude());
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		Toast.makeText(this, "Enabled new provider " + provider,
+				Toast.LENGTH_SHORT).show();
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		Toast.makeText(this, "Disabled provider " + provider,
+				Toast.LENGTH_SHORT).show();
+	}
 }
